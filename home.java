2d093ea2493b103a111c5cd484c770dd6a65b2a6
Shreyash Redkar <shreyashr202@gmail.com>
2026-06-05 18:29:59 +0530

    Update home.java

--- home.java
+++ home.java
@@ -2,7 +2,7 @@ class Demo
 {
     public static void main (String args[])
     {
-        system.out.println("this is my java file";)
+        system.out.println("this is my java file");
     }
 
 }
